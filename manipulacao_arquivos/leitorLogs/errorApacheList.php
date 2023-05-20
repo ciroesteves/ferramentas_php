fbe9113422fbe07380488adf79d4a569dd908284
ciroesteves <ciro.esteves@hotmail.com>
2023-05-20 12:41:26 -0300

    Relatório finalizado com subdivisões

--- manipulacao_arquivos/leitorLogs/errorApacheList.php
+++ manipulacao_arquivos/leitorLogs/errorApacheList.php
@@ -1,11 +1,33 @@
 <?php
 $path = "error.log";
 $arquivo = fopen($path, "r");
-$erros = array();
+$warns = $errors = $notices = $others = array();
+$countErrors = $countNotices = $countWarns = $countOthers = 0;
+$tabelaErros = $detalhesErros = $tabelaWarns = $detalhesWarns = $tabelaNotices = $detalhesNotices = $tabelaOthers = $detalhesOthers = "";
+
+function geraRelatorio($dados, $tipo)
+{
+    $tabela = "<table> 
+            <thead>
+                <tr><th width='200px'>Data/Hora</th><th width='100px'>Tipo</th><th width='500px'>Mensagem</th><th width='450px'>Arquivo</th><th width='100px'>Linha</th></tr>
+            </thead>";
+    $tabela .= "<tbody'>";
+    foreach ($dados as $dado) {
+        $tabela .= "<tr>
+    <td align='center'>{$dado['timestamp']}</td>
+    <td align='center'>{$dado['tipo']}</td>
+    <td>{$dado['mensagem']}</td>
+    <td>{$dado['arquivo']}</td>
+    <td align='center'>{$dado['linha']}</td>
+    </tr>";
+    }
+    $tabela .= "</tbody></table>";
+    return $tabela;
+}
 
 while (!feof($arquivo)) {
+    // Pegando trechos da linha
     $linha = fgets($arquivo);
-    // preg_match(pattern, input, matches, flags, offset)
     if (preg_match('/\[php\:(.+?)\]/', $linha, $matches)) {
         $tipoErro = $matches[1];
     }
@@ -13,51 +35,138 @@ while (!feof($arquivo)) {
         $dataHora = strtotime($matches[1] . ' ' . $matches[2]);
         $dataHoraErro = strftime('%d/%m/%Y %H:%M:%S', $dataHora);
     }
-
     if (preg_match('/line ([\d.:]+)/', $linha, $matches)) {
         $linhaErro = $matches[1];
     }
-
     if (preg_match('/\: (.+) in/', $linha, $matches)) {
         $mensagemErro = $matches[1];
     }
-
     if (preg_match('/in (.+) on/', $linha, $matches)) {
         $referer = $matches[1];
     }
-    
-    $erros[] = array(
-        "timestamp"=>$dataHoraErro,
-        "tipo"=>$tipoErro,
-        "linha"=>$linhaErro,
-        "mensagem"=>$mensagemErro,
-        "arquivo"=>$referer
+    // Salvando os trechos em suas respectivas subdivisões
+    if (strpos($tipoErro, "warn") !== false) {
+        $warns[] = array(
+            "timestamp" => $dataHoraErro,
+            "tipo" => $tipoErro,
+            "linha" => $linhaErro,
+            "mensagem" => $mensagemErro,
+            "arquivo" => $referer
+        );
+        $countWarns++;
+    } else if (strpos($tipoErro, "notice") !== false) {
+        $notices[] = array(
+            "timestamp" => $dataHoraErro,
+            "tipo" => $tipoErro,
+            "linha" => $linhaErro,
+            "mensagem" => $mensagemErro,
+            "arquivo" => $referer
+        );
+        $countNotices++;
+    } else if (strpos($tipoErro, "error") !== false) {
+        $errors[] = array(
+            "timestamp" => $dataHoraErro,
+            "tipo" => $tipoErro,
+            "linha" => $linhaErro,
+            "mensagem" => $mensagemErro,
+            "arquivo" => $referer
+        );
+        $countErrors++;
+    } else {
+        $others[] = array(
+            "timestamp" => $dataHoraErro,
+            "tipo" => $tipoErro,
+            "linha" => $linhaErro,
+            "mensagem" => $mensagemErro,
+            "arquivo" => $referer
+        );
+        $countOthers++;
+    }
+}
+fclose($arquivo);
+
+if(!empty($errors)){
+    $ultimoMensagemError = end($errors);
+    $tabelaErros = geraRelatorio($errors, 'error');
+    $detalhesErros = "error";
+}else {
+    $ultimoMensagemError = array(
+        "timestamp" => "-",
+        "mensagem" => "",
+        "arquivo" => ""
+    );
+}
+if(!empty($warns)){
+    $ultimoMensagemWarn = end($warns);
+    $tabelaWarns = geraRelatorio($warns, 'warn');
+    $detalhesWarns = "warn";
+}else {
+    $ultimoMensagemWarn = array(
+        "timestamp" => "-",
+        "mensagem" => "",
+        "arquivo" => ""
+    );
+}
+if(!empty($notices)){
+    $ultimoMensagemNotice = end($notices);
+    $tabelaNotices = geraRelatorio($notices, 'notice');
+    $detalhesNotices = "notice";
+}else {
+    $ultimoMensagemNotice = array(
+        "timestamp" => "-",
+        "mensagem" => "",
+        "arquivo" => ""
+    );
+}
+if(!empty($others)){
+    $ultimoMensagemOther = end($others);
+    $tabelaOthers = geraRelatorio($others, 'other');
+    $detalhesOthers = "other";
+}else {
+    $ultimoMensagemOther = array(
+        "timestamp" => "-",
+        "mensagem" => "",
+        "arquivo" => ""
     );
 }
 
-fclose($arquivo);
+echo    "<h1>Relatório de Erros do Apache</h1>
+        <div>
+        <table>
+            <thead>
+            <tr><th>Tipo</th><th>Quantidade</th><th>Data/Hora Último</th><th colspan='2'>Mensagem Completa</th></tr>
+            </thead>
+            <tbody>
+            <tr><th><a href='#' onclick='verDetalhes({$detalhesErros})'>Fatal Errors</a></th><th>{$countErrors}</th><th>{$ultimoMensagemError['timestamp']}</th><th colspan='2'>{$ultimoMensagemError['mensagem']}" . " - " . "{$ultimoMensagemError['arquivo']}</th></tr>
+            <tr id='{$detalhesErros}' style='display: none;'><td colspan='5'>{$tabelaErros}</tr></td>
+            <tr><th><a href='#' onclick='verDetalhes({$detalhesWarns})'>Warnings</a></th><th>{$countWarns}</th><th>{$ultimoMensagemWarn['timestamp']}</th><th colspan='2'>{$ultimoMensagemWarn['mensagem']}" . " - " . "{$ultimoMensagemWarn['arquivo']}</th></tr>
+            <tr id='{$detalhesWarns}' style='display: none;'><td colspan='5'>{$tabelaWarns}</tr></td>
+            <tr><th><a href='#' onclick='verDetalhes({$detalhesNotices})'>Notices</a></th><th>{$countNotices}</th><th>{$ultimoMensagemNotice['timestamp']}</th><th colspan='2'>{$ultimoMensagemNotice['mensagem']}" . " - " . "{$ultimoMensagemNotice['arquivo']}</th></tr>
+            <tr id='{$detalhesNotices}' style='display: none;'><td colspan='5'>{$tabelaNotices}</tr></td>
+            <tr><th><a href='#' onclick='verDetalhes({$detalhesOthers})'>Outros</a></th><th>{$countOthers}</th><th>{$ultimoMensagemOther['timestamp']}</th><th colspan='2'>{$ultimoMensagemOther['mensagem']}" . " - " . "{$ultimoMensagemOther['arquivo']}</th></tr>
+            <tr id='{$detalhesOthers}' style='display: none;'><td colspan='5'>{$tabelaOthers}</tr></td>
+            </tbody>
+        </table>
+        </div>";
 
-echo "<h1>Relatório de Erros do Apache</h1>";
-echo "<table>";
-echo "<tr><th width='200px'>Data/Hora</th><th width='100px'>Tipo</th><th width='500px'>Mensagem</th><th width='450px'>Arquivo</th><th width='100px'>Linha</th></tr>";
-
-$background = '';
-foreach ($erros as $erro) {
-    echo "<tr>";
-    echo "<td align='center'>" . $erro["timestamp"] . "</td>";
-    echo "<td align='center'>" . $erro["tipo"] . "</td>";
-    echo "<td>" . $erro["mensagem"] . "</td>";
-    echo "<td>" . $erro["arquivo"] . "</td>";
-    echo "<td align='center'>" . $erro["linha"] . "</td>";
-    echo "</tr>";
-}
-echo "</table>";
 ?>
+
+<script>
+    function verDetalhes(id) {
+        if(id){
+            if (id.style.display === "none") {
+            id.style.display = "";
+        } else {
+            id.style.display = "none";
+        }
+        }      
+    }
+</script>
 
 <style>
     table {
         border-collapse: collapse;
-        width: 90%;
+        width: 100%;
         margin: auto;
     }
 
